1270c00a702415e09955e08632be6f213de6f9a2
Arefnmy <arefnemayandeh@gmail.com>
2023-08-05 01:30:55 +0330

    Add Carol as LiteCoin Participant

--- examples/participant.py
+++ examples/participant.py
@@ -1,12 +1,14 @@
-from bitcoinutils.keys import PrivateKey
+from bitcoinutils.keys import PrivateKey as BTCPrivateKey
+from litecoinutils.keys import PrivateKey as LTCPrivateKey
 
 
 class Participant:
-    def __init__(self, private_key=PrivateKey()):
+    def __init__(self, private_key):
         self.private_key = private_key
         self.public_key = private_key.get_public_key()
         self.address = self.public_key.get_address()
 
 
-ALICE = Participant()
-BOB = Participant()
+ALICE = Participant(BTCPrivateKey())
+BOB = Participant(BTCPrivateKey())
+CAROL = Participant(LTCPrivateKey())
