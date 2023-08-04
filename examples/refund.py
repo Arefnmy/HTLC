6136f113058b5264757ee501eb1bf0e8df0aecfb
Arefnmy <arefnemayandeh@gmail.com>
2023-08-05 01:29:41 +0330

    TODO ScriptSig

--- examples/refund.py
+++ examples/refund.py
@@ -12,7 +12,7 @@ AMOUNT = 0.09
 END_TIME = 1691151553
 ALICE_SECRET = Secret.from_string('Alice Secret')
 
-alice_htlc = HTLC('testnet', ALICE_SECRET.secret_hash_hex(),
+alice_htlc = HTLC('btc-test', ALICE_SECRET.secret_hash_hex(),
                   ALICE.address, BOB.address, END_TIME)
 txin = TxInput(TX_ID, UTXO_INDEX)
 
@@ -21,6 +21,7 @@ txout = TxOutput(to_satoshis(AMOUNT), ALICE.address.to_script_pub_key())
 tx = Transaction([txin], [txout])
 
 sig = ALICE.private_key.sign_input(tx, 0, alice_htlc.script)
-txin.script_sig = Script([sig, alice_htlc.script.to_hex()])
+txin.script_sig = Script([sig, alice_htlc.script.to_hex()])  # TODO
 
 print(tx.get_txid())
+print(tx)
