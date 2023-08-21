a134a42a0c5c279325bf078641070be18ac08977
Arefnmy <arefnemayandeh@gmail.com>
2023-08-22 02:45:14 +0330

    update participants

--- examples/participant.py
+++ examples/participant.py
@@ -1,5 +1,7 @@
 from bitcoinutils.keys import PrivateKey as BTCPrivateKey
+from bitcoinutils.setup import setup as BTC_setup
 from litecoinutils.keys import PrivateKey as LTCPrivateKey
+from litecoinutils.setup import setup as LTC_setup
 
 
 class Participant:
@@ -9,6 +11,9 @@ class Participant:
         self.address = self.public_key.get_address()
 
 
-ALICE = Participant(BTCPrivateKey())
-BOB = Participant(BTCPrivateKey())
-CAROL = Participant(LTCPrivateKey())
+BTC_setup('testnet')
+LTC_setup('testnet')
+
+ALICE = Participant(BTCPrivateKey.from_wif('cNLwCLQe7AVtTBALB2mmAohD9qyTS3wtN6fQszoaJUyhgCgqwskW'))
+BOB = Participant(BTCPrivateKey.from_wif('cQWgi89zVdiwnJLmQRKPquFkoJ36avdsKbpvMW5gLC2HtVjdBbd1'))
+CAROL = Participant(LTCPrivateKey.from_wif('TBSVWXcs9ttsRhBHHv1hmULnj2W32aGkH9jVGYvWGDLjpjkAxEqK'))
